6b58d62087bc0ab01be22edc94c3b077f2ba4402
IThirst <annisaraihanah2@gmail.com>
2025-11-25 13:59:48 +0700

    update frontend homepage, faqs, callcenter, find us

--- Frontend/src/js/slider.js
+++ Frontend/src/js/slider.js
@@ -6,9 +6,18 @@ document.addEventListener("DOMContentLoaded", function () {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 5000,
+      speed: 800,
+      delay: 4000,
       disableOnInteraction: false,
     },
+
+    effect: "slide",               // atau coba "fade" / "cube" / "coverflow" kalau mau variasi
+    fadeEffect: {                  // kalau pakai effect: "fade"
+      crossFade: true
+    },
+    grabCursor: true,
+    transitionTimingFunction: "cubic-bezier(0.25, 0.46, 0.45, 0.94)",
+    
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
--- Frontend/src/js/temukan-kami.js
+++ Frontend/src/js/temukan-kami.js
@@ -1,5 +1,5 @@
 // Import our custom CSS
-import '../scss/main.scss'
+import '../scss/temukan-kami.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
